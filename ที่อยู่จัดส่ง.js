f6dab2855a2973129e2155df9dfe9b59c0f44ac1
siripanwalee <150766452+siripanwalee@users.noreply.github.com>
2024-02-29 00:22:10 +0700

    Add files via upload

---
+++ ที่อยู่จัดส่ง.js
@@ -0,0 +1,25 @@
+const DispTXT = document.getElementById('Res');
+
+function TxtChanged(val){
+    const TB = document.getElementById('NameTxt').value;
+    console.log(TB);
+    DispTXT.innerText = TB;
+}
+
+function เข้าสู่ระบบ() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST","")
+
+}
+
+function ดำเนินการต่อไปยังวิธีชำระเงิน() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST","")
+    
+}
+
+function ใช้() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST","")
+    
+}
